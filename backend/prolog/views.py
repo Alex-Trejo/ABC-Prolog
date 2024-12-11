8920cd86fdc76a2bc52fde65da256281c0bff944
MrBowis <02alejo20@gmail.com>
2024-12-11 09:05:57 -0500

    Peticiones desde front

--- backend/prolog/views.py
+++ backend/prolog/views.py
@@ -1,14 +1,22 @@
 from django.shortcuts import render
+from rest_framework.serializers import Serializer, CharField, IntegerField
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import status
 from pyswip import Prolog
 import os
+import json
+
+class ItemSerializer(Serializer):
+    ID = CharField()
+    Descripcion = CharField()
+    EPSS = CharField()
+    Version = CharField()
 
 # Create your views here.
 class Complete_Search(APIView):
 
-    def get(self, request):
+    def post(self, request):
         prolog = Prolog()
 
         # Obtén la ruta absoluta al archivo se.pl
@@ -20,11 +28,20 @@ class Complete_Search(APIView):
 
         res = list(prolog.query(f'tiene_vulnerabilidades("{so}", Vulnerabilidades).'))
 
-        return Response(res[0]["Vulnerabilidades"])
+        llaves = ["ID", "Descripcion", "EPSS", "Version"]
+        # Convertir lista de listas a lista de diccionarios
+        lista_diccionarios = [dict(zip(llaves, sublista)) for sublista in res[0]["Vulnerabilidades"]]
+        # Convertir a JSON
+        
+        serial = ItemSerializer(lista_diccionarios, many=True).data
+        data = {"status": "success", "items": serial}
+        
+        return Response(data)
     
+
 class Search_by_Risk(APIView):
 
-    def get(self, request):
+    def post(self, request):
         prolog = Prolog()
 
         # Obtén la ruta absoluta al archivo se.pl
@@ -35,5 +52,12 @@ class Search_by_Risk(APIView):
         EPSS = request.data.get("epss")
 
         res = list(prolog.query(f'analisis_explotabilidad({EPSS}, Lista).'))
+        llaves = ["ID", "Descripcion", "EPSS", "Version"]
+        # Convertir lista de listas a lista de diccionarios
+        lista_diccionarios = [dict(zip(llaves, sublista)) for sublista in res[0]["Lista"]]
+        # Convertir a JSON
+        
+        serial = ItemSerializer(lista_diccionarios, many=True).data
+        data = {"status": "success", "items": serial}
 
-        return Response(res[0]["Lista"])
+        return Response(data)
--- proyecto-prolog/src/app/Home/page.tsx
+++ proyecto-prolog/src/app/Home/page.tsx
@@ -4,33 +4,63 @@ import Image from "next/image";
 import { useState, useEffect } from "react";
 import axios from "axios";
 
+const data = {
+  status: "success",
+  items: [
+    {
+      ID: "b'25'",
+      Descripcion: "b'The seunshare_mount function in sandbox/seunshare.c in seunshare in certain Red Hat packages of policycoreutils 2.0.83 and earlier in Red Hat Enterprise Linux (RHEL) 6 and earlier, and Fedora 14 and earlier, mounts a new directory on top of /tmp without assigning root ownership and the sticky bit to this new directory, which allows local users to replace or delete arbitrary /tmp files, and consequently cause a denial of service or possibly gain privileges, by running a setuid application that relies on /tmp, as demonstrated by the ksu application.'",
+      EPSS: "b'44'",
+      Version: "b''"
+    },
+    {
+      ID: "b'26'",
+      Descripcion: "b'Another vulnerability description here.'",
+      EPSS: "b'50'",
+      Version: "b'1.0'"
+    }
+  ]
+};
+
 export default function Home() {
 
   const [vulnerabilities, setVulnerabilities] = useState([]);
-  const [os, setOs] = useState("");
-  const [epss, setEpss] = useState("");
+  const [os, setOs] = useState("Windows 10");
+  const [epss, setEpss] = useState("0");
+
+  const cleanValue = (value) => {
+    // Elimina los prefijos y sufijos 'b' y '
+    if (typeof value === "string") {
+      return value.replace(/^b'/, "").replace(/'$/, "");
+    }
+    return value;
+  };
 
   const getVulnerabilities = async () => {
     try {
-      axios("http://127.0.0.1:8000/complete/", {
-        method: "GET",
-        headers: {
-          "Content-Type": "application/json",
-        },
-        data: {
-          os: "Windows 10",
-        },
-      }
-      )
-      .then((response) => {
-        setVulnerabilities(response.data);
-        console.log(response.data);
-      })
-      .catch((error) => {
-        console.error(error);
+      const response = await axios.post('http://localhost:8000/complete/', {
+        so: os
       });
+      console.log('os:', os);
+      console.log('Response:', response.data);
+      return response.data;
     } catch (error) {
-      console.error(error);
+      console.error('Error fetching vulnerabilities:', error);
+      throw error;
+    }
+  };
+
+  const getVulnerabilitiesRisk = async () => {
+    try {
+      const response = await axios.post('http://localhost:8000/risk/', {
+        epss: epss
+      });
+      console.log('epss:', epss);
+      console.log('Response:', response.data);
+      return response.data;
+    } catch (error) {
+      console.error('Error fetching vulnerabilities:', error);
+      throw error;
     }
   }
 
@@ -78,19 +108,19 @@ export default function Home() {
             onChange={(e) => setOs(e.target.value)}
             className="text-gray-500 w-full border border-gray-300 rounded-md p-2 focus:outline-none focus:ring-2 focus:ring-pink-500 hover:transition duration-100 ease-in-out"
           >
-            <option value="windows 10" className="text-gray-500">Windows 10</option>
+            <option value="Windows 10" className="text-gray-500">Windows 10</option>
             <option value="Mac Os X" className="text-gray-500">Mac Os X</option>
             <option value="Macos" className="text-gray-500">Macos</option>
             {/* Fedora */}
-            <option value="fedora" className="text-gray-500">Fedora</option>
+            <option value="Fedora" className="text-gray-500">Fedora</option>
             {/* Android */}
-            <option value="android" className="text-gray-500">Android</option>
+            <option value="Android" className="text-gray-500">Android</option>
             {/* Manjaro Linux */}
-            <option value="manjaro linux" className="text-gray-500">Manjaro Linux</option>
+            <option value="Manjaro Linux" className="text-gray-500">Manjaro Linux</option>
             {/* Ubuntu Linux */}
-            <option value="ubuntu linux" className="text-gray-500">Ubuntu Linux</option>
+            <option value="Ubuntu Linux" className="text-gray-500">Ubuntu Linux</option>
             {/* Solaris */}
-            <option value="solaris" className="text-gray-500">Solaris</option>
+            <option value="Solaris" className="text-gray-500">Solaris</option>
             {/* Debian */}
 
 
@@ -123,7 +153,11 @@ export default function Home() {
                   Busqueda Completa
         </button>   {/* ruta a ocupar http://127.0.0.1:8000/complete/
 */}
-        <button className="bg-pink-700 text-white px-4 py-2 rounded-md hover:scale-110 transition delay-100 ease-in-out duration-300 hover:-translate-y-1">Analisis Alto Riesgo</button>  {/* Analisis Alto Riesgo http://127.0.0.1:8000/risk/*/}
+        <button className="bg-pink-700 text-white px-4 py-2 rounded-md hover:scale-110 transition delay-100 ease-in-out duration-300 hover:-translate-y-1"
+                onClick={getVulnerabilitiesRisk}
+        >
+                Analisis Alto Riesgo
+        </button>  {/* Analisis Alto Riesgo http://127.0.0.1:8000/risk/*/}
       </div>
 
       <div className="w-full max-w-4xl overflow-x-auto">
@@ -138,20 +172,14 @@ export default function Home() {
             </tr>
           </thead>
           <tbody>
-            <tr>
-              <td className="border border-gray-300 px-4 py-2">Vuln1</td>
-              <td className="border border-gray-300 px-4 py-2">Descripción de la vulnerabilidad 1</td>
-              <td className="border border-gray-300 px-4 py-2">Alta</td>
-              <td className="border border-gray-300 px-4 py-2">Vuln1</td>
-              
+          {data.items.map((item, index) => (
+            <tr key={index} className="text-center">
+              <td className="border border-gray-300 px-4 py-2">{cleanValue(item.ID)}</td>
+              <td className="border border-gray-300 px-4 py-2">{cleanValue(item.Descripcion)}</td>
+              <td className="border border-gray-300 px-4 py-2">{cleanValue(item.EPSS)}</td>
+              <td className="border border-gray-300 px-4 py-2">{cleanValue(item.Version)}</td>
             </tr>
-            <tr className="">
-              <td className="border border-gray-300 px-4 py-2">Vuln2</td>
-              <td className="border border-gray-300 px-4 py-2">Descripción de la vulnerabilidad 2</td>
-              <td className="border border-gray-300 px-4 py-2">Media</td>
-              <td className="border border-gray-300 px-4 py-2">Vuln1</td>
-              
-            </tr>
+          ))}
           </tbody>
         </table>
       </div>
